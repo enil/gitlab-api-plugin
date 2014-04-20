bf0deb841e369560b664f9679a4c4eb814593ddb
Emil Nilsson <eonilsson@gmail.com>
2014-04-20 16:36:38 +0200

    Allow methods in FileHelpers to take variant suffix parameter
    Changes to methods in FileHelpers:
    
    * Add a variant parameter for custom suffixes to be added to the file
    names, for instance "admin" to signify a JSON file with the information
    for an admin user.
    * Automatically add the .json file extension, that way it's possible to
    specify the path exactly the way it appears on a real GitLab API.
    * Throw FileNotFoundException if a file is missing.

--- src/test/java/com/sonymobile/gitlab/helpers/FileHelpers.java
+++ src/test/java/com/sonymobile/gitlab/helpers/FileHelpers.java
@@ -29,9 +29,12 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 
 import static org.apache.commons.io.FileUtils.readFileToString;
+import static org.apache.commons.lang.StringUtils.isNotEmpty;
 
 /**
  * Methods for loading JSON files from the test resources.
@@ -43,55 +46,114 @@ import static org.apache.commons.io.FileUtils.readFileToString;
 public class FileHelpers {
     /**
      * Loads a JSON object from a file in the test resources.
-     * 
-     * The path must be relative to the test/resources/__files directory used by WireMock.
+     *
+     * @see #getAbsoluteResourceFilePath(String, String)
      *
      * @param relativePath a path relative to the files directory
      * @return a JSON object
      * @throws IOException if the file cannot be loaded
      */
-    public static JSONObject loadJsonObjectFromFile(String relativePath) throws IOException {
-        // fixme: handle NPE gracefully
-        return new JSONObject(readFileToString(new File(getAbsoluteResourceFilePath(relativePath))));
+    public static JSONObject loadJsonObjectFromFile(String relativePath)
+            throws IOException {
+        return loadJsonObjectFromFile(relativePath, null);
+    }
+
+    /**
+     * Loads a JSON object from a file in the test resources.
+     *
+     * @see #getAbsoluteResourceFilePath(String, String)
+     *
+     * @param relativePath a path relative to the files directory
+     * @param variant the name of the variant suffix or null
+     * @return a JSON object
+     * @throws IOException if the file cannot be loaded
+     */
+    public static JSONObject loadJsonObjectFromFile(String relativePath, String variant)
+            throws IOException {
+        return new JSONObject(readFileToString(new File(getAbsoluteResourceFilePath(relativePath, variant))));
     }
 
     /**
      * Loads a JSON object from a JSON array from a file in the test resources.
      *
-     * The path must be relative to the test/resources/__files directory used by WireMock.
+     * @see #getAbsoluteResourceFilePath(String, String)
      *
      * @param relativePath the path relative to the files directory
      * @param indexInArray the index in the array
      * @return a JSON object
      * @throws IOException if the file cannot be loaded
      */
-    public static JSONObject loadJsonObjectFromFile(String relativePath, int indexInArray) throws IOException {
+    public static JSONObject loadJsonObjectFromFile(String relativePath, int indexInArray)
+            throws IOException {
         return loadJsonArrayFromFile(relativePath).getJSONObject(indexInArray);
+    }
+
+    /**
+     * Loads a JSON object from a JSON array from a file in the test resources.
+     *
+     * @see #getAbsoluteResourceFilePath(String, String)
+     *
+     * @param relativePath the path relative to the files directory
+     * @param indexInArray the index in the array
+     * @return a JSON object
+     * @throws IOException if the file cannot be loaded
+     */
+    public static JSONObject loadJsonObjectFromFile(String relativePath, int indexInArray, String variant)
+            throws IOException {
+        return loadJsonArrayFromFile(relativePath, variant).getJSONObject(indexInArray);
     }
 
     /**
      * Loads a JSON array from a file in the test resources.
      *
-     * The path must be relative to the test/resources/__files directory used by WireMock.
+     * @see #getAbsoluteResourceFilePath(String, String)
      *
      * @param relativePath a path relative to the files directory
      * @return a JSON array
      * @throws IOException if the file cannot be loaded
      */
-    public static JSONArray loadJsonArrayFromFile(String relativePath) throws IOException {
-        // fixme: handle NPE gracefully
-        return new JSONArray(readFileToString(new File(getAbsoluteResourceFilePath(relativePath))));
+    public static JSONArray loadJsonArrayFromFile(String relativePath)
+            throws IOException {
+        return loadJsonArrayFromFile(relativePath, null);
+    }
+
+    /**
+     * Loads a JSON array from a file in the test resources.
+     *
+     * @see #getAbsoluteResourceFilePath(String, String)
+     *
+     * @param relativePath a path relative to the files directory
+     * @param variant the name of the variant suffix or null
+     * @return a JSON array
+     * @throws IOException if the file cannot be loaded
+     */
+    public static JSONArray loadJsonArrayFromFile(String relativePath, String variant)
+            throws IOException {
+        return new JSONArray(readFileToString(new File(getAbsoluteResourceFilePath(relativePath, variant))));
     }
 
     /**
      * Gets the absolute file path for a test resource file.
      *
-     * The path must be relative to the test/resources/__files directory used by WireMock.
-
-     * @param relativePath a path relative to the files directory
+     * The variant is added as a suffix after a "_" to the path if set and extension ".json".
+     * The path file must be relative to the src/test/resources/__files directory used by WireMock.
+     *
+     * @param relativePath the path relative to the files directory
+     * @param variant the name of the variant suffix or null
      * @return the absolute file path
+     * @throws IOException if the file cannot be loaded
      */
-    private static String getAbsoluteResourceFilePath(String relativePath) throws IOException {
-        return FileHelpers.class.getResource("/__files/" + relativePath).getFile();
+    private static String getAbsoluteResourceFilePath(String relativePath, String variant)
+            throws IOException {
+        if (isNotEmpty(variant)) {
+            relativePath += "_" + variant;
+        }
+        relativePath = "/__files/" + relativePath + ".json";
+
+        URL url = FileHelpers.class.getResource(relativePath);
+        if (url == null) {
+            throw new FileNotFoundException("The file " + relativePath + " doesn't exist in test resources");
+        }
+        return url.getFile();
     }
 }
